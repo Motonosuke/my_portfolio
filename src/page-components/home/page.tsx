23b18372aa0f0da5390f22200d0a1063b06bb356
Motonosuke <67644458+Motonosuke@users.noreply.github.com>
2022-10-20 12:49:53 +0900

    udpate:empty message

--- src/page-components/home/page.tsx
+++ src/page-components/home/page.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react';
 
+import { Box } from 'components/chakra-parts/Layout';
 import { Text } from 'components/chakra-parts/Typography';
 
 export const HomePage: FC = () => {
-  return <Text>aa</Text>;
+  return (
+    <Box>
+      <Text>ホーム画面です</Text>
+    </Box>
+  );
 };
--- src/page-components/panelist/page.tsx
+++ src/page-components/panelist/page.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react';
 
+import { Box } from 'components/chakra-parts/Layout';
 import { Text } from 'components/chakra-parts/Typography';
 
 export const PanelistPage: FC = () => {
-  return <Text>aa</Text>;
+  return (
+    <Box>
+      <Text>登壇経験はまだありません</Text>
+    </Box>
+  );
 };
