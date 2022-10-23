c3cf39ebf75a7d71530d2c650d927226a34e37c3
Motonosuke <67644458+Motonosuke@users.noreply.github.com>
2022-10-23 11:21:53 +0900

    fix:build

--- src/page-components/self-introduction/index.ts
+++ src/page-components/self-introduction/index.ts
@@ -1,5 +1,5 @@
-export { SelfIntroductionPage } from './Page';
 export { Work } from './Work';
 export { Skill } from './Skill';
 export { BasicInformation } from './BasicInformation';
 export { Profile } from './Profile';
+export { SelfIntroductionPage } from './Page';
