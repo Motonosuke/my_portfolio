5c5787470a60889290213c6f55a33ee1137a66ea
Motonosuke <67644458+Motonosuke@users.noreply.github.com>
2022-10-30 10:51:26 +0900

    delete:unused code

--- src/constants/route-paths.ts
+++ src/constants/route-paths.ts
@@ -4,20 +4,10 @@ export const ROUTE_PATHS = {
   GITHUB_URL: 'https://github.com/Motonosuke',
   HOME: '/',
   INTRODUCTION: '/self-introduction',
-  LOGIN: '/login',
   NOGIZAKA_URL: 'https://nogizaka-radio.com',
   NOTE: '/note',
   PANELIST: '/panelist',
   PORTFOLIO_URL: 'https://motonosuke.com',
-  SETTING: {
-    ACCOUNT: '/setting/account',
-    EMAIL: '/setting/email',
-  },
   TWITTER_URL: 'https://twitter.com/motofreecall',
   YOUTUBE_CHANNEL_URL: 'https://www.youtube.com/channel/UCFp5T-QmCGCg-1KJ92Y-QHg',
 } as const;
-
-type SettingRouteType = typeof ROUTE_PATHS.SETTING;
-type SettingRoute = SettingRouteType[keyof SettingRouteType];
-
-export type RoutePathType = typeof ROUTE_PATHS.LOGIN | SettingRoute;
